b2409ad6ca9ffdb70a023bcd6c993dd4b9c0f833
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-04-11 15:51:01 +0100

    debuggin the animation

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,13 +1,15 @@
-var sr = ScrollReveal({
-	origin   : "bottom",
-	distance : "64px",
-	duration : 800,
-	delay    : 0,
-	scale    : 1
-});
+if (typeof ScrollReveal !== 'undefined') {
+	var sr = ScrollReveal({
+		origin   : "bottom",
+		distance : "64px",
+		duration : 800,
+		delay    : 0,
+		scale    : 1
+	});
 
-sr.reveal('.projects-list a');
-sr.reveal('.posts-list a');
+	sr.reveal('.projects-list a');
+	sr.reveal('.posts-list a');
+}
 
 var keywordContainer = document.querySelector('.likes-keywords');
 
@@ -19,36 +21,16 @@ if (keywordContainer) {
 		})
 		.filter(Boolean);
 	var currentKeyword = keywordContainer.querySelector('.likes-keyword-current');
-	var nextKeyword = keywordContainer.querySelector('.likes-keyword-next');
 	var keywordIndex = 0;
 	var displayDuration = 3000;
-	var transitionDuration = 550;
-	var isAnimating = false;
 
-	if (currentKeyword && nextKeyword && keywords.length > 0) {
+	if (currentKeyword && keywords.length > 0) {
 		currentKeyword.textContent = keywords[0];
-		nextKeyword.textContent = '';
 
 		if (keywords.length > 1) {
 			window.setInterval(function () {
-				var nextIndex;
-
-				if (isAnimating) {
-					return;
-				}
-
-				nextIndex = (keywordIndex + 1) % keywords.length;
-				nextKeyword.textContent = keywords[nextIndex];
-				isAnimating = true;
-				keywordContainer.classList.add('is-animating');
-
-				window.setTimeout(function () {
-					keywordIndex = nextIndex;
-					currentKeyword.textContent = keywords[keywordIndex];
-					nextKeyword.textContent = '';
-					keywordContainer.classList.remove('is-animating');
-					isAnimating = false;
-				}, transitionDuration);
+				keywordIndex = (keywordIndex + 1) % keywords.length;
+				currentKeyword.textContent = keywords[keywordIndex];
 			}, displayDuration);
 		}
 	}
