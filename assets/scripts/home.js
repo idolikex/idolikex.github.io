13090f2097f5aad933696debd25ccddbf9149a05
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-04-11 15:35:35 +0100

    Trials to get the animation working

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -12,51 +12,37 @@ sr.reveal('.posts-list a');
 var keywordContainer = document.querySelector('.likes-keywords');
 
 if (keywordContainer) {
-	var keywordTrack = keywordContainer.querySelector('.likes-keywords-track');
-	var keywordItems = keywordTrack ? Array.prototype.slice.call(keywordTrack.querySelectorAll('.likes-keyword')) : [];
-	var keywords = keywordItems.map(function (item) {
-		return item.textContent.trim();
-	}).filter(Boolean);
+	var keywords = JSON.parse(keywordContainer.getAttribute('data-keywords') || '[]');
+	var currentKeyword = keywordContainer.querySelector('.likes-keyword-current');
+	var nextKeyword = keywordContainer.querySelector('.likes-keyword-next');
 	var keywordIndex = 0;
 	var displayDuration = 3000;
 	var transitionDuration = 550;
+	var isAnimating = false;
 
-	if (keywordTrack && keywords.length > 0) {
-		keywordTrack.innerHTML = '' +
-			'<span class="likes-keyword likes-keyword-sizer" aria-hidden="true"></span>' +
-			'<span class="likes-keyword likes-keyword-current"></span>' +
-			'<span class="likes-keyword likes-keyword-next"></span>';
-
-		var keywordSizer = keywordTrack.querySelector('.likes-keyword-sizer');
-		var currentKeyword = keywordTrack.querySelector('.likes-keyword-current');
-		var nextKeyword = keywordTrack.querySelector('.likes-keyword-next');
-
-		var measureTicker = function () {
-			var longestKeyword = keywords.reduce(function (longest, keyword) {
-				return keyword.length > longest.length ? keyword : longest;
-			}, keywords[0]);
-
-			keywordSizer.textContent = longestKeyword;
-			keywordContainer.style.width = keywordSizer.offsetWidth + 'px';
-			keywordContainer.style.height = keywordSizer.offsetHeight + 'px';
-		};
-
+	if (currentKeyword && nextKeyword && keywords.length > 0) {
 		currentKeyword.textContent = keywords[0];
-		nextKeyword.textContent = keywords[1] || keywords[0];
-		measureTicker();
-		window.addEventListener('resize', measureTicker);
+		nextKeyword.textContent = '';
 
 		if (keywords.length > 1) {
 			window.setInterval(function () {
-				var nextIndex = (keywordIndex + 1) % keywords.length;
+				var nextIndex;
 
+				if (isAnimating) {
+					return;
+				}
+
+				nextIndex = (keywordIndex + 1) % keywords.length;
 				nextKeyword.textContent = keywords[nextIndex];
-				keywordTrack.classList.add('is-animating');
+				isAnimating = true;
+				keywordContainer.classList.add('is-animating');
 
 				window.setTimeout(function () {
 					keywordIndex = nextIndex;
 					currentKeyword.textContent = keywords[keywordIndex];
-					keywordTrack.classList.remove('is-animating');
+					nextKeyword.textContent = '';
+					keywordContainer.classList.remove('is-animating');
+					isAnimating = false;
 				}, transitionDuration);
 			}, displayDuration);
 		}
