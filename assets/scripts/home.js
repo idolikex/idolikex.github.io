cbc7f43b6a5495987e054c164c1a10be858c993d
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-04-11 13:31:13 +0100

    Trial with animation

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -8,3 +8,19 @@ var sr = ScrollReveal({
 
 sr.reveal('.projects-list a');
 sr.reveal('.posts-list a');
+
+var keywordContainer = document.querySelector('.likes-keywords');
+
+if (keywordContainer) {
+	var keywordTrack = keywordContainer.querySelector('.likes-keywords-track');
+	var keywords = JSON.parse(keywordContainer.getAttribute('data-keywords') || '[]');
+	var keywordIndex = 0;
+	var rotationDelay = 1800;
+
+	if (keywordTrack && keywords.length > 1) {
+		window.setInterval(function () {
+			keywordIndex = (keywordIndex + 1) % keywords.length;
+			keywordTrack.style.transform = 'translateY(-' + keywordIndex + 'em)';
+		}, rotationDelay);
+	}
+}
