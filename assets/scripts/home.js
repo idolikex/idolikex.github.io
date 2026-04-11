ba84858d18caf3874d9449261642f97cbfc2f57c
Divij Ghose <67919237+divijghose@users.noreply.github.com>
2026-04-11 13:36:33 +0100

    Chnages to animation

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -13,14 +13,45 @@ var keywordContainer = document.querySelector('.likes-keywords');
 
 if (keywordContainer) {
 	var keywordTrack = keywordContainer.querySelector('.likes-keywords-track');
-	var keywords = JSON.parse(keywordContainer.getAttribute('data-keywords') || '[]');
+	var keywordItems = keywordTrack ? Array.prototype.slice.call(keywordTrack.querySelectorAll('.likes-keyword')) : [];
 	var keywordIndex = 0;
-	var rotationDelay = 1800;
+	var rotationDelay = 2200;
+	var transitionDuration = 650;
+	var keywordHeight = 0;
 
-	if (keywordTrack && keywords.length > 1) {
+	if (keywordTrack && keywordItems.length > 0) {
+		var measureTicker = function () {
+			var maxWidth = 0;
+
+			keywordItems.forEach(function (item) {
+				maxWidth = Math.max(maxWidth, item.offsetWidth);
+			});
+
+			keywordHeight = keywordItems[0].offsetHeight;
+			keywordContainer.style.width = maxWidth + 'px';
+			keywordContainer.style.height = keywordHeight + 'px';
+			keywordTrack.style.transform = 'translateY(-' + (keywordIndex * keywordHeight) + 'px)';
+		};
+
+		measureTicker();
+		window.addEventListener('resize', measureTicker);
+	}
+
+	if (keywordTrack && keywordItems.length > 1) {
+		keywordTrack.appendChild(keywordItems[0].cloneNode(true));
+
 		window.setInterval(function () {
-			keywordIndex = (keywordIndex + 1) % keywords.length;
-			keywordTrack.style.transform = 'translateY(-' + keywordIndex + 'em)';
+			keywordIndex += 1;
+			keywordTrack.style.transition = 'transform ' + transitionDuration + 'ms ease';
+			keywordTrack.style.transform = 'translateY(-' + (keywordIndex * keywordHeight) + 'px)';
+
+			if (keywordIndex === keywordItems.length) {
+				window.setTimeout(function () {
+					keywordTrack.style.transition = 'none';
+					keywordIndex = 0;
+					keywordTrack.style.transform = 'translateY(0)';
+				}, transitionDuration);
+			}
 		}, rotationDelay);
 	}
 }
